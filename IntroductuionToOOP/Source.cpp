70b99c4aa1d58dd3446611a1f24cf916a414609b
Mikhail Sherkunkov <sherkunkov@gmail.com>
2023-06-25 22:40:14 +0800

    2. done

--- IntroductuionToOOP/Source.cpp
+++ IntroductuionToOOP/Source.cpp
@@ -5,40 +5,50 @@ using namespace std;
 //#define STRUCT_POINT
 //#define DISTACE_CHECK
 //#define CONSTRUCTORS_CHECK
+//#define ASSIGNMENT_CHECK 
 //#define INCREMENT_CHECK
 #define delimiter "\n----------------------\n"
+
+class Point;
+Point operator+(const Point& left, const Point& right);
+Point operator-(const Point& left, const Point& right);
+Point operator*(const Point& left, const Point& right);
+Point operator/(const Point& left, const Point& right);
+ostream& operator<<(ostream& os, const Point& obj);
+istream& operator>>(istream& os, Point& obj);
+double distance(const Point& A, const Point& B);
 
 class Point {
 	double x;
 	double y;
 public:
-	
-	void print(){
+
+	void print() {
 		cout << getX() << "\t" << getY() << endl;
 	}
-	void setX(double x){
+	void setX(double x) {
 		this->x = x;
 	}
-	void setY(double y){
+	void setY(double y) {
 		this->y = y;
 	}
-	double getX()const{
+	double getX()const {
 		return x;
 	}
-	double getY()const{
+	double getY()const {
 		return y;
 	}
-							//Counstructors:
-	/*Point() {
-		x = y = double(); // - значение по умолчанию для заданного типа
-		//таким образом мы якобы вызываем конструктор по умолчанию для заданного типа
-		cout << "DefoultCountructor:\t" << this << endl;
-	}
-	Point(double x) {
-		this->x = x;
-		this->y = 0;
-		cout << "1ArgCountructor:\t" << this << endl;
-	}*/
+	//Counstructors:
+/*Point() {
+	x = y = double(); // - значение по умолчанию для заданного типа
+	//таким образом мы якобы вызываем конструктор по умолчанию для заданного типа
+	cout << "DefoultCountructor:\t" << this << endl;
+}
+Point(double x) {
+	this->x = x;
+	this->y = 0;
+	cout << "1ArgCountructor:\t" << this << endl;
+}*/
 
 	Point(double x = 0, double y = 0) {
 		this->x = x;
@@ -53,8 +63,8 @@ public:
 	~Point() {
 		cout << "Destructor:\t" << this << endl;
 	}
-							//Operators
-	Point& operator=(const Point& other){
+	//Operators
+	Point& operator=(const Point& other) {
 		this->x = other.x;
 		this->y = other.y;
 		cout << "CopyAssignment:\t" << this << endl;
@@ -63,6 +73,21 @@ public:
 	Point& operator+=(const Point& other) {
 		this->x += other.x;
 		this->y += other.y;
+		return *this;
+	}
+	Point& operator-=(const Point& other) {
+		this->x -= other.x;
+		this->y -= other.y;
+		return *this;
+	}
+	Point& operator*=(const Point& other) {
+		this->x *= other.x;
+		this->y *= other.y;
+		return *this;
+	}
+	Point& operator/=(const Point& other) {
+		this->x /= other.x;
+		this->y /= other.y;
 		return *this;
 	}
 	Point& operator++() {
@@ -75,16 +100,80 @@ public:
 		x++;
 		y++;
 		return old;
-	}						//Methods
-	double distance(const Point& other)const{
+	}
+	Point& operator--() {
+		x--;
+		y--;
+		return *this;
+	}
+	Point operator--(int) {
+		Point old = *this;
+		x--;
+		y--;
+		return old;
+	}
+						//Methods
+	double distance(const Point& other)const {
 		double x_distace = this->x - other.x;
 		double y_distace = this->y - other.y;
 		double distance = sqrt(x_distace * x_distace + y_distace * y_distace);
 		return distance;
 	}
 };
-ostream& operator<<(ostream& os, const Point obj) {
+							//Functions
+Point operator+(const Point& left, const Point& right){
+	/*Point result;
+	result.setX(left.getX() + right.getX());
+	result.setY(left.getY() + right.getY());*/
+	/*Point result
+	(
+	left.getX() + right.getX(), 
+	left.getY() + right.getY()
+	);
+	return result;*/
+	return Point
+	(
+		left.getX() + right.getX(),
+		left.getY() + right.getY()
+	);
+}
+Point operator-(const Point& left, const Point& right){
+	return Point
+	(
+		left.getX() - right.getX(),
+		left.getY() - right.getY()
+	);
+}
+Point operator*(const Point& left, const Point& right){
+	return Point
+	(
+		left.getX() * right.getX(),
+		left.getY() * right.getY()
+	);
+}
+Point operator*(const Point& left, const double& right) {
+	return Point
+	(
+		left.getX() * right,
+		left.getY() * right
+	);
+}
+Point operator/(const Point& left, const Point& right){
+	return Point
+	(
+		left.getX() / right.getX(),
+		left.getY() / right.getY()
+	);
+}
+ostream& operator<<(ostream& os, const Point& obj) {
 	return os << "X = " << obj.getX() << "\tY = " << obj.getY();
+}
+istream& operator>>(istream& os, Point& obj) {
+	double x;
+	double y;
+	cin >> x >> y;
+	obj.setX(x), obj.setY(y);
+	return os;
 }
 double distance(const Point& A, const Point& B) {
 	double x_distace = A.getX() - B.getX();
@@ -147,8 +236,7 @@ void main(){
 	E.print();
 
 #endif // !CONSTRUCTORS_CHECK
-#ifdef INCREMENT_CHECK
-
+#ifdef ASSIGNMENT_CHECK
 	int a, b, c = 0;
 	a = b = c = 0;
 	cout << a << "\t" << b << "\t" << c << "\t" << endl;
@@ -161,14 +249,56 @@ void main(){
 	A = B++;
 	A.print();
 	B.print();
+#endif // !ASSIGNMENT_CHECK
+#ifdef INCREMENT_CHECK
+
+	int a = 2;
+	int b = 3;
+	int c = a + b;
+
+	cout << c << endl;
+
+	Point A (2, 3);
+	Point B (7, 8);
+	Point C = A + B;
+	cout << C << endl;
 
 #endif // !INCREMENT_CHECK
-	Point A (2, 3);
-	A.print();
-
-	Point B(7, 8);
-	B.print();
-
-	A;
+	Point A;
+	
+	Point B;
+	
+	cin >> A >> B;
 	cout << A << endl;
+	
+	cout << B << endl;
+
+	A = A * 2;
+	cout << A << endl;
+	A++;
+	cout << "Postincrement A: " << A << endl;
+	++A;
+	cout << "Preincrement A: " << A << endl;
+	A--;
+	cout << "Postdecrement A: " << A << endl;
+	--A;
+	cout << "Predecrement A: " << A << endl;
+	A += B;
+	cout << "A += B: " << A << endl;
+	A -= B;
+	cout << "A -= B: " << A << endl;
+	A *= B;
+	cout << "A *= B: " << A << endl;
+	A /= B;
+	cout << "A /= B: " << A << endl;
+
+	Point C;
+	C = A + B;
+	cout << "A += B: " << C << endl;
+	C = A - B;
+	cout << "A -= B: " << C << endl;
+	C = A * B;
+	cout << "A *= B: " << C << endl;
+	C = A / B;
+	cout << "A /= B: " << C << endl;
 }
