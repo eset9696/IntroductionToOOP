290336ada102275957173f7e59ff2086a393ff6f
Mikhail Sherkunkov <sherkunkov@gmail.com>
2023-06-29 21:27:26 +0800

    operators done

--- Class Fraction/Source.cpp
+++ Class Fraction/Source.cpp
@@ -2,12 +2,25 @@
 
 using namespace std;
 
-class Fraction{
+class Fraction;
+int calcNOD(int a, int b);
+Fraction operator+(Fraction left, Fraction right);
+Fraction operator-(Fraction left, Fraction right);
+Fraction operator*(Fraction left, Fraction right);
+Fraction operator/(Fraction left, Fraction right);
+bool operator<(Fraction left, Fraction right);
+bool operator>(const Fraction& left, const Fraction& right);
+bool operator==(Fraction left, Fraction right);
+bool operator!=(const Fraction& left, const Fraction& right);
+bool operator<=(Fraction left, Fraction right);
+bool operator>=(const Fraction& left, const Fraction& right);
+
+class Fraction {
 	int integer;
 	int numerator;
 	int denominator;
 public:
-	int getInteger()const{
+	int getInteger()const {
 		return integer;
 	}
 	int getNumerator()const {
@@ -16,7 +29,7 @@ public:
 	int getDenominator()const {
 		return denominator;
 	}
-	void setInteger(int integer){
+	void setInteger(int integer) {
 		this->integer = integer;
 	}
 	void setNumerator(int numerator) {
@@ -45,19 +58,19 @@ public:
 		setDenominator(denominator);
 		cout << "2 Parameters Constructor:\t" << this << endl;
 	}
-	Fraction(int integer, int numerator, int denominator){
+	Fraction(int integer, int numerator, int denominator) {
 		this->integer = integer;
 		this->numerator = numerator;
 		setDenominator(denominator);
 		cout << "3 Parameters Constructor:\t" << this << endl;
 	}
-	Fraction(const Fraction& other){
+	Fraction(const Fraction& other) {
 		this->integer = other.getInteger();
 		this->numerator = other.getNumerator();
 		setDenominator(other.getDenominator());
 		cout << "Copy constructor:\t\t" << this << endl;
 	}
-	~Fraction(){ cout << "Destructor:\t\t\t" << this << endl; }
+	~Fraction() { cout << "Destructor:\t\t\t" << this << endl; }
 	//Operators
 	Fraction& operator=(const Fraction& other) {
 		this->integer = other.getInteger();
@@ -67,36 +80,45 @@ public:
 		return *this;
 	}
 	Fraction& operator+=(const Fraction& other) {
-		this->integer += other.getInteger();
+		/*this->integer += other.getInteger();
 		this->numerator = this->numerator * other.getDenominator() + other.getNumerator() * this->denominator;
 		this->denominator *= other.getDenominator();
-		this->reduce();
-		return *this;
+		this->reduce();*/
+		return *this = *this + other;
 	}
 	Fraction& operator-=(const Fraction& other) {
-		int buffer = other.getNumerator() + other.getInteger() * other.getDenominator();
+		/*int buffer = other.getNumerator() + other.getInteger() * other.getDenominator();
 		this->toImproper();
 		this->numerator = this->numerator * other.getDenominator() - buffer * this->denominator;
 		this->denominator *= other.getDenominator();
 		this->reduce();
+		return *this;*/
+		*this = *this - other;
+		reduce();
 		return *this;
 	}
 	Fraction& operator*=(const Fraction& other) {
-		int buffer = other.getNumerator() + other.getInteger() * other.getDenominator();
+		/*int buffer = other.getNumerator() + other.getInteger() * other.getDenominator();
 		this->toImproper();
 		this->numerator = this->numerator * buffer;
 		this->denominator *= other.getDenominator();
 		this->reduce();
-		return *this;
+		return *this;*/
+		return *this = *this * other;
 	}
-
 	Fraction& operator/=(const Fraction& other) {
-		int buffer = other.getNumerator() + other.getInteger() * other.getDenominator();
+		/*int buffer = other.getNumerator() + other.getInteger() * other.getDenominator();
 		this->toImproper();
 		this->numerator = this->numerator * other.getDenominator();
 		this->denominator *= buffer;
 		this->reduce();
-		return *this;
+		return *this;*/
+		return *this = *this / other;
+	}
+	Fraction& operator-() {
+		toImproper();
+		numerator *= -1;
+		return this->toProper();
 	}
 	Fraction& operator++() { //Prefix increment
 		integer++;
@@ -107,7 +129,7 @@ public:
 		integer++;
 		return old;
 	}
-	Fraction& operator--(){
+	Fraction& operator--() {
 		integer--;
 		return *this;
 	}
@@ -117,9 +139,9 @@ public:
 		return old;
 	}
 	//Methods
-	void print(){
+	void print() {
 		if (integer) cout << integer;
-		if(numerator){
+		if (numerator) {
 			if (integer)cout << "(";
 			cout << numerator << "/" << denominator;
 			if (integer)cout << ")";
@@ -127,57 +149,110 @@ public:
 		else if (integer == 0) cout << 0;
 		cout << endl;
 	}
-	void toProper(){
-		if(this->numerator >= this->denominator){
+	Fraction& toProper() {
+		/*if(this->numerator >= this->denominator){
 			this->integer = this->integer + this->numerator / this->denominator;
 			this->numerator = this->numerator % this->denominator;
 		}else if(-this->numerator >= this->denominator){
 			this->integer = -(this->integer + -this->numerator / this->denominator);
 			this->numerator = -(this->numerator % this->denominator);
-		}
+		}*/
+		integer += numerator / denominator;
+		numerator %= denominator;
+		return *this;
 	}
-	void toImproper() {
-		if (this->numerator < this->denominator) {
-			this->numerator = this->integer * this->denominator + this->numerator;
-			this->integer = 0;
-		}
+	Fraction& toImproper() {
+		numerator += integer * denominator;
+		integer = 0;
+		return *this;
 	}
-	void reduce() {
+	Fraction Inverted()const {
+		Fraction inverted = *this;
+		inverted.toImproper();
+		std::swap(inverted.numerator, inverted.denominator);
+		return inverted;
+	}
+	Fraction& reduce() {
 		this->toProper();
-		for(int i = 2; i <= this->numerator;){
+		for (int i = 2; i <= this->numerator;) {
 			if (this->numerator % i == 0 && this->denominator % i == 0) {
 				this->numerator /= i;
 				this->denominator /= i;
 			}
 			else i++;
 		}
+		return *this;
+	}
+	Fraction& reduceEVKLD() {
+		toProper();
+		int nod = calcNOD(numerator, denominator);
+		numerator /= nod;
+		denominator /= nod;
+		return *this;
 	}
 };
 //Functions
-Fraction& operator+(const Fraction& left, const Fraction& right){
-	Fraction result = left;
-	Fraction tempR = right; //Фиг знает почему, но если удалить эту строку то в знаменатель запишется какой то мусор
-	return result += right;
+int calcNOD(int a, int b) {
+	if (a != 0) {
+		calcNOD(b % a, a);
+	}
+	else {
+		return a + b;
+	}
 }
-Fraction& operator-(const Fraction& left, const Fraction& right) {
-	Fraction result = left;
-	Fraction tempR = right;
-	return result -= right;
+Fraction operator+(Fraction left, Fraction right) {
+	left.toImproper();
+	right.toImproper();
+	return Fraction
+	(
+		left.getNumerator() * right.getDenominator() + right.getNumerator() * left.getDenominator(),
+		left.getDenominator() * right.getDenominator()
+	).toProper().reduceEVKLD();
 }
-Fraction& operator*(const Fraction& left, const Fraction& right) {
-	Fraction result = left;
-	Fraction tempR = right;
-	return result *= right;
+Fraction operator-(Fraction left, Fraction right) {
+	return Fraction(left + -right);
 }
-Fraction& operator/(const Fraction& left, const Fraction& right) {
-	Fraction result = left;
-	Fraction tempR = right;
-	return result /= right;
+Fraction operator*(Fraction left, Fraction right) {
+	left.toImproper();
+	right.toImproper();
+	return Fraction
+	(
+		left.getNumerator() * right.getNumerator(),
+		left.getDenominator() * right.getDenominator()
+	).toProper().reduceEVKLD();
+}
+Fraction operator/(Fraction left, Fraction right) {
+	return Fraction(left * right.Inverted());
+}
+bool operator<(Fraction left, Fraction right) {
+	left.toImproper();
+	right.toImproper();
+	return left.getNumerator() * right.getDenominator() < right.getNumerator() * left.getDenominator()? true : false;
+	//return left - right < 0 ? true : false;
+}
+bool operator>(const Fraction& left, const Fraction& right) {
+	return left < right ? false : true;
+}
+bool operator==(Fraction left, Fraction right) {
+	left.toImproper();
+	right.toImproper();
+	return left.getNumerator() * right.getDenominator() == right.getNumerator() * left.getDenominator()? true : false;
+}
+bool operator!=(const Fraction& left, const Fraction& right) {
+	return left == right? false : true;
+}
+bool operator<=(Fraction left,Fraction right) {
+	left.toImproper();
+	right.toImproper();
+	return left.getNumerator() * right.getDenominator() <= right.getNumerator() * left.getDenominator()? true : false;
+}
+bool operator>=(const Fraction& left, const Fraction& right) {
+	return left <= right ? false : true;
 }
 //#define CONSTRUCTORS_CHECK
-//#define METHODS_CHECK
+#define METHODS_CHECK
 //#define OPERATORS_CHECK
-#define FUNCTIONS_CHECK
+//#define FUNCTIONS_CHECK
 //#define INCREMENT_CHECK
 void main() {
 	setlocale(LC_ALL, "");
@@ -200,17 +275,42 @@ void main() {
 	Fraction F(2, 11, 5);
 	F = B;
 	F.print();
-	
+
 #endif // CONSTRUCTORS_CHECK
 #ifdef METHODS_CHECK
-	Fraction A(0, 3, 4);
+	Fraction A(2, 3, 4);
 	A.print();
-	A.reduce();
+	Fraction B(3, 4, 5);
+	Fraction C = A + B;
+	C.print();
+	C = A - B;
+	C.print();
+	A += B;
 	A.print();
-	A.toProper();
+	A -= B;
 	A.print();
-	A.toImproper();
+	A *= B;
 	A.print();
+	A /= B;
+	A.print();
+	Fraction D = A;
+	bool a;
+	a = A < B;
+	cout << "A < B: " << a << endl;
+	a = A > B;
+	cout << "A > B: " << a << endl;
+	a = A == D;
+	cout << "A == D: " << a << endl;
+	a = A != D;
+	cout << "A != D: " << a << endl;
+	a = A == B;
+	cout << "A == B: " << a << endl;
+	a = A != B;
+	cout << "A != B: " << a << endl;
+	a = A <= D;
+	cout << "A <= D: " << a << endl;
+	a = A >= B;
+	cout << "A >= B: " << a << endl;
 #endif // METHODS_CHECK
 #ifdef OPERATORS_CHECK
 	Fraction A(3, 2, 3);
@@ -233,7 +333,7 @@ void main() {
 	A.print();
 	B.print();
 	Fraction C;
-	C = A + B;
+	C = (A + B);
 	A.print();
 	B.print();
 	C.print();
